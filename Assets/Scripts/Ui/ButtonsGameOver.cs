e69aca42a819ad32ea8f653b441625dfabdeb282
felipsPrado <felips0206@gmail.com>
2025-06-29 22:30:37 -0300

    HUDgameover

---
+++ Assets/Scripts/Ui/ButtonsGameOver.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonsGameOver : MonoBehaviour
+{
+
+    public TextMeshProUGUI[] tmpButtons;
+
+    [SerializeField] private byte LoadSceneMenu;
+
+    void Start()
+    {
+
+    }
+
+    public void VoltaMenu()
+    {
+        SceneManager.LoadScene(LoadSceneMenu);
+        Debug.Log("tentando voltar");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
